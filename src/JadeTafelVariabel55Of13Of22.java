1e0cf91cd5168cca154de3613b44ac980b4d35d8
JWahyu <71404267+JWahyu@users.noreply.github.com>
2020-09-24 00:42:44 +0200

    Jade / JWahyu
    Verschillende manieren uitgeprobeerd om tafels te maken met meerdere variabelen en soorten operators.

--- src/JadeTafelVariabel55Of13Of22.java
+++ src/JadeTafelVariabel55Of13Of22.java
@@ -19,51 +19,53 @@ public class JadeTafelVariabel55Of13Of22 {
         System.out.println("9 x " + x + " = " + x*9);
         System.out.println("10 x " + x + " = " + x*10);
 
-        System.out.println();
-        System.out.println("─────────────────────────────");
-        System.out.println();
-
-        // Tafel van 13 als variabel y.
-
-        System.out.println("Tafel van 13 als variabel y:");
-        System.out.println();
-
-        int y = 13;
-
-        System.out.println("1 x " + y + " = " + y);
-        System.out.println("2 x " + y + " = " + y*2);
-        System.out.println("3 x " + y + " = " + y*3);
-        System.out.println("4 x " + y + " = " + y*4);
-        System.out.println("5 x " + y + " = " + y*5);
-        System.out.println("6 x " + y + " = " + y*6);
-        System.out.println("7 x " + y + " = " + y*7);
-        System.out.println("8 x " + y + " = " + y*8);
-        System.out.println("9 x " + y + " = " + y*9);
-        System.out.println("10 x " + y + " = " + y*10);
 
         System.out.println();
         System.out.println("─────────────────────────────");
         System.out.println();
 
-        // Tafel van 22 als variabel z.
+        // Tafel van 13 als variabel a.
 
-        System.out.println("Tafel van 22 als variabel z:");
+        System.out.println("Tafel van 13 als variabel a:");
         System.out.println();
 
-        int z = 22;
+        int a = 13;
+        int b = 1;
+        int c = 1;
 
-        System.out.println("1 x " + z + " = " + z);
-        System.out.println("2 x " + z + " = " + z*2);
-        System.out.println("3 x " + z + " = " + z*3);
-        System.out.println("4 x " + z + " = " + z*4);
-        System.out.println("5 x " + z + " = " + z*5);
-        System.out.println("6 x " + z + " = " + z*6);
-        System.out.println("7 x " + z + " = " + z*7);
-        System.out.println("8 x " + z + " = " + z*8);
-        System.out.println("9 x " + z + " = " + z*9);
-        System.out.println("10 x " + z + " = " + z*10);
+        System.out.println(b++ + " x " + a + " = " + a*c++);
+        System.out.println(b++ + " x " + a + " = " + a*c++);
+        System.out.println(b++ + " x " + a + " = " + a*c++);
+        System.out.println(b++ + " x " + a + " = " + a*c++);
+        System.out.println(b++ + " x " + a + " = " + a*c++);
+        System.out.println(b++ + " x " + a + " = " + a*c++);
+        System.out.println(b++ + " x " + a + " = " + a*c++);
+        System.out.println(b++ + " x " + a + " = " + a*c++);
+        System.out.println(b++ + " x " + a + " = " + a*c++);
+        System.out.println(b + " x " + a + " = " + a*c);
 
         System.out.println();
+        System.out.println("─────────────────────────────");
+        System.out.println();
+
+        // Tafel van 22 als variabel d.
+
+        System.out.println("Tafel van 22 als variabel y:");
+        System.out.println();
+
+        int d = 22;
+        int e = 1;
+
+        System.out.println(e + " x " + d + " = " + d*e++);
+        System.out.println(e + " x " + d + " = " + d*e++);
+        System.out.println(e + " x " + d + " = " + d*e++);
+        System.out.println(e + " x " + d + " = " + d*e++);
+        System.out.println(e + " x " + d + " = " + d*e++);
+        System.out.println(e + " x " + d + " = " + d*e++);
+        System.out.println(e + " x " + d + " = " + d*e++);
+        System.out.println(e + " x " + d + " = " + d*e++);
+        System.out.println(e + " x " + d + " = " + d*e++);
+        System.out.println(e + " x " + d + " = " + d*e);
 
     }
 }
